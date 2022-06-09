2081ed7d86793ea41f8f1392f4603eb650416f5e
Jacob Martinson <jacobm3@gmail.com>
2022-06-09 12:27:19 -0500

    Update instance-state.sh

--- terraform/instance-state.sh
+++ terraform/instance-state.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 
 echo -n "State: "
-aws ec2 describe-instances --instance-ids $(terraform output --raw vault_instance_id) \
+aws ec2 describe-instances --instance-ids \
+$(terraform output --raw vault_instance_id) \
+$(terraform output --raw syslog_instance_id) \
  | jq -r ".Reservations[] | .Instances[] | .State.Name"
 
-aws ec2 describe-instances --query "Reservations[].Instances[].[Tags[?Key=='Name'],InstanceId,State.Name]"  | jq
+#aws ec2 describe-instances --query "Reservations[].Instances[].[Tags[?Key=='Name'],InstanceId,State.Name]"  | jq
