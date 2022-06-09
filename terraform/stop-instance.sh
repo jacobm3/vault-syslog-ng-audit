a10801bec45946434028a46beb2b7e0ff8b6795e
Jacob Martinson <jacobm3@gmail.com>
2022-06-09 12:25:58 -0500

    Update ec2 stop script.

--- terraform/stop-instance.sh
+++ terraform/stop-instance.sh
@@ -1,4 +1,3 @@
 #!/bin/bash -x
 
-aws ec2 stop-instances --instance-ids $(terraform output --raw vault_instance_id)
-
+aws ec2 stop-instances --instance-ids $(terraform output --raw vault_instance_id) $(terraform output --raw syslog_instance_id)
