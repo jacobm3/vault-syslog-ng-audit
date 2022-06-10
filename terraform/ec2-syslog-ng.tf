5be6ccff1e353c7c43de7f7631921c74b07c208a
Jacob Martinson <jacobm3@gmail.com>
2022-06-10 12:32:18 -0500

    update

--- terraform/ec2-syslog-ng.tf
+++ terraform/ec2-syslog-ng.tf
@@ -1,20 +1,6 @@
 resource "aws_instance" "syslog" {
   ami = data.aws_ami.latest-ubuntu.id
 
-  # arm64
-  # t4g.nano: 2vpu, 512MB ram, ebs = $0.0042/hr = $0.10/day = $3/month
-  # t4g.micro: 2vpu, 1GB ram, ebs = $0.0084/hr = $0.20/day = $6/month
-  # t4g.medium: 2vpu, 4GB ram, ebs = $0.0336/hr = $0.81/day = $24/month
-  # t4g.large: 2vpu, 8GB ram, ebs = $0.0672/hr = $1.61/day = $48/month
-
-  # amd64
-  # t3a.nano; 2 vcpu, 512MB ram, ebs = $0.0052/hr = $0.12/day = $3.70/month
-  # t3a.large; 2 vcpu, 8GB ram, ebs = $1.80/day
-  # m5d.large; 2 vcpu, 8GB ram, nvme = $2.70/day
-  # m5d.xlarge; 4 vcpu, 16GB ram, nvme = $5.42/day
-  # m6a.large; 2 vcpu, 8GB ram, ebs = $2.07/day
-  # c6a.xlarge: 4 vcpu, 8GB ram, ebs = $3.67/day
-
   instance_type = var.syslog_instance_type
   key_name      = var.key_name
 
